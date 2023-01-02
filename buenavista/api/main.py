2ad26c1a17dd702d2d7f50c5e7115af66992367e
Josh Wills <josh.wills@gmail.com>
2023-01-01 20:48:12 -0800

    Make this work a tiny bit better

--- buenavista/api/main.py
+++ buenavista/api/main.py
@@ -5,13 +5,22 @@ import duckdb
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+# Use this to serve a public/index.html
+from starlette.responses import FileResponse 
 
 from buenavista.backend.duckdb import DuckDBAdapter
 from buenavista.core import BuenaVistaServer
 from buenavista.extensions import dbt
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory=os.path.dirname(__file__) + "/static", html=True), name="static")
+STATIC_FILE_DIR = os.path.join(os.path.dirname(__file__), "static")
+
+app.mount(
+    "/static",
+    StaticFiles(directory=STATIC_FILE_DIR),
+    name="static",
+)
+
 
 @app.on_event("startup")
 def startup():
@@ -27,11 +36,15 @@ def startup():
     bv_thread.start()
 
 
+@app.get("/")
+def index():
+    return FileResponse(os.path.join(STATIC_FILE_DIR, "index.html"))
+
+
 class QueryRequest(BaseModel):
     sql: str
 
-
-@app.post("/query")
+@app.post("/api/query")
 def query(q: QueryRequest):
     handle = app.bv.adapter.create_handle()
     query_result = handle.execute_sql(q.sql)
